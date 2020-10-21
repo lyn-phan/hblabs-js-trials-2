a2096338708af7a86373aeabcf6b9bff951e7fef
vagrant <vagrant@vagrant.vm>
2020-10-21 00:59:08 +0000

    Finished arrays

--- arrays.js
+++ arrays.js
@@ -3,17 +3,29 @@
 
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
+  for (const i in items) {
+    console.log(`${items[i]} ${i}`);
+  }
 }
-
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  const result = [];
+  for (const i in items) {
+    if (i % 2 === 0) {
+      result.push(items[i]);
+    }
+  }
+  console.log(result);
 }
 
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+  const sortedItems = items.sort((a, b) => a - b).slice(0, n);
+  sortedItems.reverse(); 
+  console.log(sortedItems);
 }
+
+// # states.slice(0, -1) 
+// # states [: -1]
