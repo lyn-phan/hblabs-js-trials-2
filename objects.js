af4e234436a749c148f8a846d56e62435f554921
vagrant <vagrant@vagrant.vm>
2020-10-21 04:19:12 +0000

    Completed objects

--- objects.js
+++ objects.js
@@ -1,13 +1,28 @@
 "use strict";
 
-
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+  const wordCounts = {};
+  for (const word of phrase.split('')) {
+    if (wordCounts[word]) {
+    wordCounts[word] += 1;
+  } else {
+    wordCounts[word] = 1;
 }
-
+}
+return wordCounts;
+}
 
 // 2. getMelonsAtPrice
 function getMelonsAtPrice(price) {
-  // Replace this with your code
+  const melonPrices = {
+    2.50: ['Cantaloupe', 'Honeydew'],
+    2.95: ['Watermelon'],
+    3.25: ['Musk', 'Crenshaw'],
+    14.25: ['Christmas']
+  };
+  if (!melonPrices[price]) {
+    return;
+  }
+  return melonPrices[price].sort();
 }
